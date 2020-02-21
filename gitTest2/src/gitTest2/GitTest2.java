85d49323e1900243910d41e902459120768ee636
soup930 <58681922+soup930@users.noreply.github.com>
2020-02-21 14:46:48 +0900

    Update GitTest2.java

--- gitTest2/src/gitTest2/GitTest2.java
+++ gitTest2/src/gitTest2/GitTest2.java
@@ -2,6 +2,6 @@ package gitTest2;
 
 public class GitTest2 {
 	public static void main(String[] args) {
-		System.out.println("ddddddddddddddddddddddddddddddddddd");
+		System.out.println("dicivnwoxocv9d");
 	}
 }
