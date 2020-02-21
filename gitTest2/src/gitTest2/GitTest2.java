9744f32368f896c796344764ea3183d2126ea5b3
krg <soup930@naver.com>
2020-02-21 15:00:25 +0900

    ccc

--- gitTest2/src/gitTest2/GitTest2.java
+++ gitTest2/src/gitTest2/GitTest2.java
@@ -3,7 +3,7 @@ package gitTest2;
 public class GitTest2 {
 	public static void main(String[] args) {
 
-		System.out.println("dicivnwoxocv9d");
+		System.out.println("dicivzxczxcnwoxocv9d");
 
 	}
 }
